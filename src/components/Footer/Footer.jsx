56b5572daffbe9ee22f20901b313ac0b42dc6d3b
Chloe Casteran <chloe.casteran@gmail.com>
2025-07-15 12:12:19 +0200

    footer jsx and scss add in Home

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './footer.scss';
+
+const Footer = () => {
+    return(
+        <div className="footer">
+            <p className="footer__text">Copyright © Casteran Chloë</p>
+        </div>
+    )
+};
+
+export default Footer;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import Header from '../../components/Header/Header';
 import Featured from '../../components/Featured/Featured';
 import MangaRead from '../../components/MangaRead/MangaRead';
+import Footer from '../../components/Footer/Footer';
 import './home.scss';
 
 const Home = () => {
@@ -13,6 +14,7 @@ const Home = () => {
             <div className='mangaRead'>
                 <MangaRead />
             </div>
+            <Footer />
         </div>
     )
 };
